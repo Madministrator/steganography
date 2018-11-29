893585b47dfcc673076c5605cf429dca6a02e580
fergesen <45017714+fergesen@users.noreply.github.com>
2018-11-29 14:14:39 -0600

    added isbitset function

--- JavaScript Proof of Concept/BitManipulation.js
+++ JavaScript Proof of Concept/BitManipulation.js
@@ -1,6 +1,10 @@
 //TO BE IMPLEMENTED
 isBitSet = function(byte, index) {
-
+    index = Number(index); //make sure input was a number
+    if(byte[index] == true)
+        return true
+    else
+        return false
 }
 setBit = function(byte, index, value) {
 
