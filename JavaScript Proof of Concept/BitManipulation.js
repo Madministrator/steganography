053c888673ecdb4364930638e8d0ab504fe31330
fergesen <fergesen.c@gmail.com>
2018-12-03 22:35:48 -0600

    fixed isbitset and setbit
    Changed isbitset and setbit to work with a number instead of a string

--- JavaScript Proof of Concept/BitManipulation.js
+++ JavaScript Proof of Concept/BitManipulation.js
@@ -1,13 +1,23 @@
 //TO BE IMPLEMENTED
 isBitSet = function(byte, index) {
-    index = Number(index); //make sure input was a number
-    if(byte[index] == true)
+    // Align the bit at the index spot to the least significant bit
+    byte = byte >> (index - 1)
+
+    // Check if this number is odd. If it is then the bit is set
+    if(byte % 1 == 1)
         return true
     else
         return false
 }
 setBit = function(byte, index, value) {
-    byte[index] = value
+    // Value should be 0 or 1
+    let mask = value << index
+    if (value == 1)
+        // or byte with mask to set the value bit in byte to 1
+        byte | mask
+    else 
+        // and byte with mask to set the value bit in byte to 0
+        byte & mask
 }
 getByte = function(number, index) {
     let mask = 0x000000FF
