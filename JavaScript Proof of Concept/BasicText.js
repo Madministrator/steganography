ac6ee5384290f41f53dd80c2c63580dbabde7e16
Evan Vander Hoeven <vhevan.927@gmail.com>
2018-12-04 14:14:15 -0600

    removed innaccurate comment

--- JavaScript Proof of Concept/BasicText.js
+++ JavaScript Proof of Concept/BasicText.js
@@ -1,6 +1,5 @@
 class BasicText {
     /*
-    length = the number of characters in the textfile
     data = Uint8ClampedArray of the characters encoded in the Text file
     */
     cosntructor(data) {
