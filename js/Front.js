76ae4a2b5cd45fa68d5354907e84281fdff8cb46
Nicholas LaBelle <nrlabelle@gmail.com>
2018-12-06 13:43:17 -0600

    Update Front.js

--- js/Front.js
+++ js/Front.js
@@ -4,17 +4,12 @@ var inputImageLabel = document.getElementById('ImageLabel');
 var inputHiddenLabel = document.getElementById('HiddenLabel');
 var originalImage = document.getElementById('original');
 var changedImage = document.getElementById('changed');
-var greedSlider =document.getElementById('greed')
-var greed;
 
 inputImage.addEventListener('change', function(){applyName(inputImage,inputImageLabel)});
 
 
 inputHidden.addEventListener('change',function(){applyName(inputHidden, inputHiddenLabel)});
 
-greedSlider.addEventListener('change',function(){
-	greed = greedSlider.value;
-});
 function applyName(input,label) {
 	 let file = input.files[0];
 
