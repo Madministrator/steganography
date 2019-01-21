d6f59c637b7a2af4b1f59490d234525c888219a8
Evan Vander Hoeven <vhevan.927@gmail.com>
2019-01-21 14:34:04 -0600

    Modfied previous code to bootstrap
    The same functionality is provided, but now has the bootstrap appearance. The greediness selection object, and the ability to display images pending. The steganography algorithms are no longer linked due to changes in naming conventions.

--- js/Front.js
+++ js/Front.js
@@ -6,7 +6,7 @@ var originalImage = document.getElementById('original');
 var changedImage = document.getElementById('changed');
 document.getElementById('greed').addEventListener('change', function(){
 	document.getElementById('greedNumber').innerHTML = document.getElementById('greed').value;
-})
+});
 
 inputImage.addEventListener('change', function(){applyName(inputImage,inputImageLabel)});
 
